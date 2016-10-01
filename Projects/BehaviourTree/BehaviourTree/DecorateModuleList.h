a61abf213090e0a5dcb56451558266e68b5c6d37
Sun-Lay Gagneux <gags1920@usherbrooke.ca>
2016-10-01 16:03:42 -0400

    An manager include header for decorate modules
    Not tested
    
    I made an include list for including every decorate module in one row

---
+++ Projects/BehaviourTree/BehaviourTree/DecorateModuleList.h
@@ -0,0 +1,9 @@
+#ifndef DECORATEMODULELIST_H_INCLUDED
+#define DECORATEMODULELIST_H_INCLUDED
+
+
+#include "InvertDModule.h"
+#include "DecorationModule.h"
+
+
+#endif //!DECORATEMODULELIST_H_INCLUDED
